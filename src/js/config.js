82abe518b355b35b323f710357dbb2da916eb3c1
AbdoHesham22 <abdo.hesham1452000@gmail.com>
2023-12-14 13:09:24 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '23203ff2-fe9b-4db3-89ac-11d388fc04ba';
+export const MODAL_CLOSE_SEC = 2.5;
